946b44ddc8bff85d934a390acd2b5b283029e02d
JulioNunesDev <julionunes999@gmail.com>
2022-07-24 03:16:55 -0300

    add: fundo_sprits

--- spt_main.py
+++ spt_main.py
@@ -26,16 +26,54 @@ class Sapo(pygame.sprite.Sprite):
         self.sprites.append(pygame.image.load('sprites/attack_8.png')) 
         self.sprites.append(pygame.image.load('sprites/attack_9.png')) 
         self.sprites.append(pygame.image.load('sprites/attack_10.png')) 
+        self.atual = 0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image, (128*3,64*3))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 200,70
+
+        self.animar = False
+    
+    def atacar(self):
+        self.animar = True
+
+    def update(self):
+        if self.animar == True:
+            self.atual += 0.2
+            if self.atual >= len(self.sprites):
+                self.atual = 0
+                self.animar = False
+            self.image = self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image, (128*3,64*3))
+
+
+
+image_fundo = pygame.image.load('sprites_fundo/fundo.jpg').convert()
+image_fundo = pygame.transform.scale(image_fundo, (largura, altura) )
+
+
+frames = pygame.time.Clock()
+
+todas_as_sprites = pygame.sprite.Group()
+sapo = Sapo()
+todas_as_sprites.add(sapo)
+
 
 
 
 
 
 while True:
+    frames.tick(60)
     tela.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            sapo.atacar()
 
+    tela.blit(image_fundo, (0,0))
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
     pygame.display.flip()
